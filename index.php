28fbf6acd335ef64ca1e68348c265a7820b67be0
super <lexmengual@gmail.com>
2019-10-04 20:00:04 +0200

    sudoku

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Sudoku</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+    <body>
+        <div class="content">
+          <table>
+            <?php
+              $cell_num = 1;
+              $cells = array();
+              for ($i=0; $i < 9; $i++) {
+                if ($i == 2 || $i == 5) {
+                  echo "<tr class='bBottom'>";
+                } else {
+                  echo "<tr>";
+                }
+                for ($j=0; $j < 9; $j++) {
+                  if ($j == 2 || $j == 5) {
+                    echo "<td id='cell".$cell_num."' class='bRight'></td>";
+                  } else {
+                    echo "<td id='cell".$cell_num."'></td>";
+                  }
+                  array_push($cells, $cell_num);
+                  $cell_num += 1;
+                }
+                echo "</tr>";
+              }
+              for ($i=0; $i < 20; $i++) {
+                $index = rand(0, count($cells)-1);
+                $num = rand(1, 9);
+                $cell_pos = $cells[$index];
+                echo "<script>document.getElementById('cell".$cell_pos."').innerHTML = ".$num."</script>";
+                unset($cells[$index]);
+                $cells = array_values($cells);
+              }
+             ?>
+          </table>
+        </div>
+    </body>
+</html>
